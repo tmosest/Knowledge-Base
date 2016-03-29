3e750cea540024aaf20a6172891929df49425829
Tyler Moses <tmosest@gmail.com>
2016-03-29 11:04:31 -0400

    Create set-cookie.js

---
+++ JavaScript/set-cookie.js
@@ -0,0 +1,8 @@
+// Simple function to set a cookie by the name, value, and desired time in seconds
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
