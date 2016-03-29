f56289081649e8835d9f567b253bf80420121a75
Tyler Moses <tmosest@gmail.com>
2016-03-29 13:11:43 -0400

    Update set-cookie.js

--- JavaScript/set-cookie.js
+++ JavaScript/set-cookie.js
@@ -1,5 +1,5 @@
 // Simple function to set a cookie by the name, value, and desired time in seconds
-
+// here is the code to set path document.cookie = cname + "=" + cvalue + ";path=/; " + expires;
 function setCookie(cname, cvalue, exsecs) {
     var d = new Date();
     d.setTime(d.getTime() + (exsecs*1000));
