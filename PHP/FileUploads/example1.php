dff3488fbc5e767ac13e7dacd15df6f5c48da5a2
Tyler Moses <tmosest@gmail.com>
2016-04-20 15:39:56 -0400

    Update example1.php

--- PHP/FileUploads/example1.php
+++ PHP/FileUploads/example1.php
@@ -53,7 +53,7 @@ if ($uploadOk == 0) {
 </head>
 
 <body>
-<form action="fileupload.php" method="post" enctype="multipart/form-data">
+<form action="example1.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
